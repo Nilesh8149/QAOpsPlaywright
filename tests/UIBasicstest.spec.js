4e2bfaaa8e3e9b9dfdee37a7f1c366ecb584cfaa
Nilesh8149 <nileshadole963@gmail.com>
2026-08-01 21:22:07 +0530

    Update name in UIBasics test file

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -3,7 +3,7 @@ const { text } = require('node:stream/consumers');
 
 test.describe.configure({mode:"parallel"});
 
-//My Name is Kajal
+//My Name is Kalpesh
 
 test('@Web Browser context Playwright Test',async ({browser})=>
 {
